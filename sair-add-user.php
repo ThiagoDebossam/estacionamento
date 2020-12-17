54bc724d2b598cd09c9a70f7d0370610061518ac
ThiagoDebossam <thiagodebossam8@gmail.com>
2020-12-17 15:20:54 -0300

    corrigindo commit anterior

--- sair-add-user.php
+++
@@ -1,6 +0,0 @@
-<?php
-//encerra sessao como administrador
-    session_start();
-    unset($_SESSION["master"]);
-    header("location:login-add-user.php");
-?>
--- sair.php
+++
@@ -1,6 +0,0 @@
-<?php
-//encerra sessao de login
-    session_start();
-    unset($_SESSION["user"]);
-    header("location:login.php");
-?>
